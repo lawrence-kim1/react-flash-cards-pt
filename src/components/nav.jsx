335e31a9b4bcb16dcbee451cf314b85510d2aaeb
Lawrence <lawrey7@gmail.com>
2020-03-04 10:54:21 -0800

    Added a NavBar component.

---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+  return(
+    <nav class="navbar navbar-light bg-light">
+      <ul>
+        <li className="nav-item">View Cards</li>
+        <li className="nav-item">Review Cards</li>
+        <li className="nav-item">Create Card</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
