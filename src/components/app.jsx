4f767c852738cadccfc858e6bc5def2adde45214
Lawrence <lawrey7@gmail.com>
2020-03-04 10:48:17 -0800

    Added a getView method to check cases with a switch.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ViewCards from './view-cards';
+import ReviewCards from './review-cards';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: ViewCards.value
+      view: 'view-cards'
     }
   }
 
   setView(cardView) {
     this.setState({view})
+  }
+
+  getView() {
+    switch {
+      case 'create-card':
+        return <CreateCard />;
+      case 'review-cards':
+        return <ReviewCards />;
+      case 'view-cards':
+        return <ViewCards />;
+      default:
+        return null;
+    }
   }
 
   render() {
