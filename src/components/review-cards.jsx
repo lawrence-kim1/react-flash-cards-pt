50fbf8df9008dba86d08f35bf447b0c251c84301
Lawrence <lawrey7@gmail.com>
2020-03-04 15:18:10 -0800

    Added a constructor and initial front state to the ReviewCards component.

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 class ReviewCards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { front: true }
+  }
 
   componentDidMount() {
     this.props.setActiveCard(0)
   }
+
   render() {
     return(
       <div className="container-sm">
