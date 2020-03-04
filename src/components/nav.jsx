fdbd94a88c56e4dd74b7671bf2e31cec8e03773b
Lawrence <lawrey7@gmail.com>
2020-03-04 12:58:31 -0800

    Changed navbar to buttons.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,13 +7,25 @@ class Nav extends React.Component {
         <div className="navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="#" className="nav-link" onClick={() => this.props.setView('view-cards')}>View Cards</a>
+              <a href="#"
+              className="nav-link btn btn-outline-primary"
+              onClick={() => this.props.setView('view-cards')}>
+                View Cards
+              </a>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link" onClick={() => this.props.setView('review-cards')}>Review Cards</a>
+              <a href="#"
+              className="nav-link btn btn-outline-primary"
+              onClick={() => this.props.setView('review-cards')}>
+                Review
+              </a>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link" onClick={() => this.props.setView('create-card')}>Create Card</a>
+              <a href="#"
+              className="nav-link btn btn-outline-primary"
+              onClick={() => this.props.setView('create-card')}>
+                Create Card
+              </a>
             </li>
           </ul>
         </div>
