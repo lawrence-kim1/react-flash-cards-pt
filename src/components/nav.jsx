c698b25bf94cab792c6f21ee3f379d4e06734da4
Lawrence <lawrey7@gmail.com>
2020-03-04 11:07:57 -0800

    Added Bootstrap styling to Nav component.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 class Nav extends React.Component {
   render(){
     return (
-      <nav className="navbar navbar-light bg-light">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <a className="nav-link" onClick={() => this.props.setView('view-cards')}>View Cards</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" onClick={() => this.props.setView('review-cards')}>Review Cards</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" onClick={() => this.props.setView('create-card')}>Create Card</a>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand navbar-light bg-light">
+        <div className="navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="#" className="nav-link" onClick={() => this.props.setView('view-cards')}>View Cards</a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link" onClick={() => this.props.setView('review-cards')}>Review Cards</a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link" onClick={() => this.props.setView('create-card')}>Create Card</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     )
   }
