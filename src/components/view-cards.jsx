63ce51ae71cbcc8ed5c342a4957ce023f67f5021
Lawrence <lawrey7@gmail.com>
2020-03-04 13:26:31 -0800

    Added a hard coded card front to display in ViewCards.

--- src/components/view-cards.jsx
+++ src/components/view-cards.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 
 class ViewCards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardFront: null
+    }
+  }
+
 
   componentDidMount() {
     const cardsArray = this.props.cards;
     console.log(cardsArray);
+    const cardFront = cardsArray.map(card => {
+      return <div>{card.question}</div>
+    });
+    this.setState( {cardFront} );
+    console.log(this.state.cardFront);
   }
 
   render() {
     return(
       <div className="container-sm">
         <h1 className="text-center">View Cards</h1>
+        {this.state.cardFront}
       </div>
     );
   }
