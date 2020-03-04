14816c64e49943765536685586f0f52400c8300e
Lawrence <lawrey7@gmail.com>
2020-03-04 10:44:28 -0800

    Added an initial state to the App component.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import ViewCards from './view-cards';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: ViewCards.value
+    }
+  }
   render() {
     return(
       <h1 className="text-center">Flash Card App</h1>
