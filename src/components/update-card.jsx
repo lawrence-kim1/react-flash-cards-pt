1424220e44c7be1176e108637b3be4262ad97cba
Lawrence <lawrey7@gmail.com>
2020-03-06 13:09:17 -0800

    Added an UpdateCard with initial styling set to Create Card form.

---
+++ src/components/update-card.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class UpdateCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: '',
+      answer: ''
+    }
+    this.handleQuestionChange = this.handleQuestionChange.bind(this);
+    this.handleAnswerChange = this.handleAnswerChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  render() {
+    return(
+      <div>
+        <h1 className="text-center">Update Card</h1>
+        <form className="container-sm" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="question">Question:</label>
+            <textarea
+              value={this.state.question}
+              onChange={this.handleQuestionChange}
+              className="form-control"
+              id="question"
+              rows="3" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="answer">Answer:</label>
+            <textarea
+              value={this.state.answer}
+              onChange={this.handleAnswerChange}
+              className="form-control"
+              id="answer"
+              rows="3" />
+          </div>
+          <div className="d-flex justify-content-end">
+            <button onClick={this.reset} className="btn btn-outline-danger mr-3">Cancel</button>
+            <button className="btn btn-outline-primary">Save Card</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default UpdateCard;
