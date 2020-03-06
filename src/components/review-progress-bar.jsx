e11108e0aa87f784c54463c7157cce76d21fa0fb
Lawrence <lawrey7@gmail.com>
2020-03-05 17:28:28 -0800

    Added a ProgressBar component.

---
+++ src/components/review-progress-bar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ProgressBar extends React.Component {
+  render() {
+    const value = 10;
+    return(
+      <div className="progress">
+        <div className="progress-bar" style={{width: `${value}`}} />
+      </div>
+    )
+  }
+}
+
+export default ProgressBar;
