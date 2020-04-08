5bda9bbc40729c2c04a26aa5e4eae41cc75a75ae
Lawrence <lawrey7@gmail.com>
2020-04-08 13:40:20 -0700

    Removed the anchor tags.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,25 +7,25 @@ class Nav extends React.Component {
         <div className="navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="#"
+              <div
               className="nav-link btn btn-outline-primary"
               onClick={() => this.props.setView('view-cards')}>
                 View Cards
-              </a>
+              </div>
             </li>
             <li className="nav-item">
-              <a href="#"
+              <div
               className="nav-link btn btn-outline-primary"
               onClick={() => this.props.setView('review-cards')}>
                 Review
-              </a>
+              </div>
             </li>
             <li className="nav-item">
-              <a href="#"
+              <div
               className="nav-link btn btn-outline-primary"
               onClick={() => this.props.setView('create-card')}>
                 Create Card
-              </a>
+              </div>
             </li>
           </ul>
         </div>
