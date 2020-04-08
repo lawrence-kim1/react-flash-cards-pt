60e546403c4f433df83972ea458f05df4b2c4cf4
Lawrence <lawrey7@gmail.com>
2020-04-08 13:36:02 -0700

    Added more styling to the modal.

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -9,14 +9,14 @@ class Modal extends React.Component{
       return (
         <div className="text-center d-flex flex-column align-items-center modal">
           <div className="modal-inner-content">
-            <div>
+            <div className="modal-bold">
               Are you sure you want to delete this card?
             </div>
             <div>
-              Q: {card.question}
+              <span className="modal-question modal-bold">Q:</span> {card.question}
             </div>
             <div>
-              A: {card.answer}
+              <span className="modal-answer modal-bold">A:</span> {card.answer}
             </div>
             <div>
               <button onClick={() => this.props.closeModal(null)} className="btn btn-outline-secondary mr-3">Cancel</button>
